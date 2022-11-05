b0747c04ef18ff88120fed46d414b8f7682a6636
simsgood2 <simsgood2@gmail.com>
2022-11-05 22:48:41 +0900

    백그라운드컬러 변경

--- JavaRunner/index.js
+++ JavaRunner/index.js
@@ -1,34 +1,46 @@
-
 let result = document.getElementById("result");
 let dropdown = document.getElementById("dropdown");
+let output_box = document.getElementById("output_box");
 let editor = ace.edit(document.getElementById("code_area"), {
-    theme: "ace/theme/cobalt",
-    mode: "ace/mode/java"
+  theme: "ace/theme/cobalt",
+  mode: "ace/mode/java",
 });
 let input_box = document.getElementById("input_box");
 
-function change_theme(){
-    editor.setTheme("ace/theme/" + dropdown.value);
+let colors = {
+  cobalt: "#052440",
+  dracula: "#303440",
+  dreamweaver: "#ffffff",
+  one_dark: "#282C34",
+  monokai: "#272822",
+  github: "#ffffff",
+};
+
+function change_theme() {
+  input_box.style.backgroundColor = colors[dropdown.value];
+  output_box.style.backgroundColor = colors[dropdown.value];
+  editor.setTheme("ace/theme/" + dropdown.value);
 }
 
-function compile_code () {
-    let url = "https://runjava.mcv.kr/runCode";
-    let body;
+function compile_code() {
+  let url = "https://runjava.mcv.kr/runCode";
+  let body;
 
-    if(input_box.value !== ""){
-        url += "WithInput"
-        body = JSON.stringify({"code":editor.getValue(),"input":input_box.value})
-    }
-    else {
-        body = editor.getValue();
-    }
+  if (input_box.value !== "") {
+    url += "WithInput";
+    body = JSON.stringify({ code: editor.getValue(), input: input_box.value });
+  } else {
+    body = editor.getValue();
+  }
 
-    fetch(url, {
-        method: "POST",
-        body: body
-    }).then(async (response) => {
-        result.textContent = await response.text()
-    }).catch((err) => console.error(err));
+  fetch(url, {
+    method: "POST",
+    body: body,
+  })
+    .then(async (response) => {
+      result.textContent = await response.text();
+    })
+    .catch((err) => console.error(err));
 }
 
 dropdown.addEventListener("change", change_theme);
